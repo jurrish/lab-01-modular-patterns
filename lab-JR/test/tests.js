af09ba8707ed947b81d525972cdce6be24c0e7c0
Frank Iriarte <jurrish@gmail.com>
2016-11-16 18:43:09 -0800

    got tests setup, now trying to figure out why this is failing

--- lab-JR/test/tests.js
+++ lab-JR/test/tests.js
@@ -1,12 +1,13 @@
 //require in function from greet.js
 //would i pass entire object to index.js through some modular pattern?
 //what are dev dependencies? are they the ones that live in package.json script: ?
-var testGreet = require('greet');
-var assert = require('assert');
+const testGreet = require('../lib/greet');
+const assert = require('assert');
 describe('function', function() {
   describe('greet()', function() {
-    it('should return a concatenated string Hello + name', function(){
-      assert.
-    })
-  })
-})
+    it('should return a string Hello + param', function(){
+      //assert.equal(actual, expected, [message]);
+      assert.equal(testGreet('JR'), 'Hello' + 'JR');
+    });
+  });
+});
